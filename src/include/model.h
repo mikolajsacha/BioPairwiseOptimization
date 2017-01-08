b528418f4f3afc2b521ae1864919274d5a04a3ff
Mikolaj Sacha <mikolajsacha@gmail.com>
2017-01-08 22:33:05 +0100

    #4 Implement basic Python test structure

--- src/include/model.h
+++ src/include/model.h
@@ -30,6 +30,8 @@ struct AlignmentBacktrace {
     int i1; /*!< Current index on first sequence axis */
     int i2; /*!< Current index on second sequence axis */
     bool isFinished; /*!< Marks that current alignment has finished backtracing */
+    int begin; /*!< Index where alignment begins */
+    int end; /*!< Index where alignment ends */
 };
 
 #endif
--- test/runtests.py
+++ test/runtests.py
@@ -1,3 +1,58 @@
+"""
+Unit tests of the shared library.
+"""
+from os import listdir
+from os.path import isfile, join
+import unittest
+from Bio import pairwise2, SeqIO
 import pairwise
+# from Bio.SubsMat import MatrixInfo #  to be used later on
 
-print pairwise.globalxx("ACCGT", "ACG")
+TEST_DATA_FOLDER = 'testdata'
+DISPLAY_ALIGNMENT_LENGTH = 15
+
+
+def test_sequences_pairs():
+    """
+    Returns generator of all test data (as tuple: (filename, seq1, seq2)) that we have stored in testdata folder
+    """
+    seq_files = (f for f in listdir(TEST_DATA_FOLDER) if f.endswith('fasta') and isfile(join(TEST_DATA_FOLDER, f)))
+    return (tuple([f] + [str(s.seq) for s in SeqIO.parse(join(TEST_DATA_FOLDER, f), 'fasta')]) for f in seq_files)
+
+def shorten_string(string, max_length):
+    """ Shorten string to given length adding ... if it is shorter"""
+    if len(string) > max_length:
+        return string[:max_length] + '...'
+    return string
+
+def format_alignment(alignment, max_length=DISPLAY_ALIGNMENT_LENGTH):
+    """ format alignment to readable form """
+    return tuple(shorten_string(str(val), max_length) for val in alignment)
+
+class TestScoreCorrectness(unittest.TestCase):
+    """
+    Tests if scores of our alignments are same as scores calculated by methods from Bio.pairwise2
+    """
+    def test_globalxx(self):
+        """
+        Tests globalxx method (no gap penalty, simply 1 point for match and 0 for mismatch)
+        """
+        for filename, seq1, seq2 in test_sequences_pairs():
+            try:
+                our_alignments = pairwise.globalxx(seq1, seq2)
+                bio_alignments = pairwise2.align.globalxx(seq1, seq2)
+                self.assertEqual(len(our_alignments), len(bio_alignments))
+            except AssertionError as err:
+                print 'Assertion error in ' + str(filename)
+                print 'Error message: ' + str(err)
+                print 'Seq1 = ' + seq1[:50]
+                print 'Seq2 = ' + seq2[:50]
+                print "Our alignments:"
+                print '\n'.join([str(format_alignment(al)) for al in our_alignments])
+                print "Bio alignments:"
+                print '\n'.join([str(format_alignment(al)) for al in bio_alignments])
+                raise err
+
+
+if __name__ == "__main__":
+    unittest.main()
